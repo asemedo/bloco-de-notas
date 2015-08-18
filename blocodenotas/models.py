0eb6464bbdfa5646076d75bf82a34d4b93680c38
António Semedo <antoniosem@gmail.com>
2015-08-18 22:13:57 +0100

    Created model Notas

--- blocodenotas/models.py
+++ blocodenotas/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 class Notas(models.Model):
 
-    class Meta:
-        verbose_name = "Nota"
-        verbose_name_plural = "Notas"
+    autor = models.ForeignKey('auth.User')
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
 
     def __unicode__(self):
-        pass
+        return self.title
