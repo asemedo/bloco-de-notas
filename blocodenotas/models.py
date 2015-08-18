7ef3cc7e621d065d53eb9774a8ab95eec3f2c622
António Semedo <antoniosem@gmail.com>
2015-08-18 18:06:27 +0100

    Added template and static folders; Added Basic URLs and Views

--- blocodenotas/models.py
+++ blocodenotas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Notas(models.Model):
+
+    class Meta:
+        verbose_name = "Nota"
+        verbose_name_plural = "Notas"
+
+    def __unicode__(self):
+        pass
--- blocodenotas/views.py
+++ blocodenotas/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
 
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView,
+)
+from django.views.generic.edit import FormView
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django import forms
+from .models import Notas
+
+
 # Create your views here.
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class Login(FormView):
+
+    template_name = 'registration/login.html'
+    form_class = LoginForm
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(self.request, user)
+                return HttpResponseRedirect(self.get_success_url())
+            else:
+                return HttpResponseRedirect(reverse('login'))
+        else:
+            return HttpResponseRedirect(reverse('login'))
+
+    def get_success_url(self):
+            return reverse('notas-lista')
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
