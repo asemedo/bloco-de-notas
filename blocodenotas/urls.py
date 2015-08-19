8fb5640bf7eb9fef83e2ff26e88130d1e582582d
António Semedo <antoniosem@gmail.com>
2015-08-19 03:06:38 +0100

    Added login, user and notas views

--- blocodenotas/urls.py
+++ blocodenotas/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.Login.as_view(), name='login'),
+    url(r'^$', views.home_view, name='home'),
+    url(r'^notaslista/$', views.NotaListView.as_view(), name='notas_lista'),
+    url(r'^userslista/$', views.UserListView.as_view(), name='users_lista'),
+    url(r'^login$', views.Login.as_view(), name='login'),
+    url(r'^logout$', views.logout_view, name='logout'),
 ]
--- blocodenotas/views.py
+++ blocodenotas/views.py
@@ -13,9 +13,20 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django import forms
 from .models import Nota
+from django.contrib.auth.models import User, Group
 
 
 # Create your views here.
+def home_view(request):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('login'))
+    else:
+        if is_member_administrator(request.user):
+            return HttpResponseRedirect(reverse('users_lista'))
+        else:
+            return HttpResponseRedirect(reverse('notas_lista'))
+
+
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
@@ -42,9 +53,39 @@ class Login(FormView):
             return HttpResponseRedirect(reverse('login'))
 
     def get_success_url(self):
-            return reverse('notas-lista')
+            return reverse('home')
 
 
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('login'))
+
+
+class NotaListView(ListView):
+    model = Nota
+    template_name = 'blocodenotas/notas_list.html'
+
+    def dispatch(self, *args, **kwargs):
+        if not self.request.user.is_authenticated():
+            return HttpResponseRedirect(reverse('login'))
+        else:
+            return super(NotaListView, self).dispatch(*args, **kwargs)
+
+
+class UserListView(ListView):
+    model = User
+    template_name = 'blocodenotas/users_list.html'
+
+    def dispatch(self, *args, **kwargs):
+        if not self.request.user.is_authenticated():
+            return HttpResponseRedirect(reverse('login'))
+        else:
+            return super(UserListView, self).dispatch(*args, **kwargs)
+
+
+def is_member_administrator(user):
+    return user.groups.filter(name='Administrador').exists()
+
+
+def is_member_utilizador(user):
+    return user.groups.filter(name='Utilizador').exists()
