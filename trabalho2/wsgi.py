2e38b31b5e3c32df0efd41cb7fb4be42c8910518
António Semedo <antoniosem@gmail.com>
2015-08-27 20:38:54 +0100

    additional files and settings for heroku

--- trabalho2/wsgi.py
+++ trabalho2/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trabalho2.settings")
 
 application = get_wsgi_application()
+
+# heroku settings
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
